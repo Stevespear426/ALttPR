794a91ee640da75548af62f66ae629b1a9973002
Steve Spear <stephen.k.spear@gmail.com>
2026-08-26 18:07:38 -0400

    Preparing Github Actions and Release builds

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -1,8 +1,23 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import java.util.Properties
 
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.composeCompiler)
+}
+
+val versionMajor = libs.versions.major.get().toInt()
+val versionMinor = libs.versions.minor.get().toInt()
+val versionBug = libs.versions.bug.get().toInt()
+val versionString = "${versionMajor}.${versionMinor}.${versionBug}"
+
+val getLocalProperty = { key: String ->
+    val properties = Properties()
+    val localPropertiesFile = rootProject.file("local.properties")
+    if (localPropertiesFile.exists()) {
+        localPropertiesFile.inputStream().use { properties.load(it) }
+    }
+    properties.getProperty(key) ?: System.getenv(key) ?: ""
 }
 
 kotlin {
@@ -25,21 +40,35 @@ android {
         applicationId = "com.stingers.alttpr"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = (versionMajor * 10000) + (versionMinor * 100) + (versionBug)
+        versionName = versionString
     }
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+    signingConfigs {
+        create("release") {
+            keyAlias = getLocalProperty("keyAlias")
+            keyPassword = getLocalProperty("keyPassword")
+            val storeFileLocalProp = getLocalProperty("storeFile")
+            if (storeFileLocalProp.isNotEmpty()) {
+                storeFile = file(storeFileLocalProp)
+            }
+            storePassword = getLocalProperty("storePassword")
+        }
+    }
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            signingConfig = signingConfigs.getByName("release")
+            ndk.debugSymbolLevel = "FULL"
         }
     }
     compileOptions {
